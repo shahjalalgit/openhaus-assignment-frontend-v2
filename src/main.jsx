b387f988a7d311ac3b1de21c64461ff4f9e4dcd7
Mohammad Shahjalal <shahjalal.csegub@gmail.com>
2023-08-15 00:03:45 +0600

    redux toolkit and emotion setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//import redux
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
