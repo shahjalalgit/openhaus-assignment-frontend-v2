d2d7db5b04e7b7eb4ad3fbd5e36dc0198709859d
Mohammad Shahjalal <shahjalal.csegub@gmail.com>
2023-08-15 10:42:05 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persitor, store } from "./redux/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CustomRouterProvider from "./routes";
 
 function App() {
