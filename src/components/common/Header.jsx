3a9f28269253be8d3b14722dfceb5c6949bbaf3e
Mohammad Shahjalal <shahjalal.csegub@gmail.com>
2023-08-16 18:33:52 +0600

    Header import updated

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+function Header({title, enableBgColor}) {
+  return (
+    <div className={`fs-4 ${enableBgColor ? 'bg-info' : 'bg-white'} d-flex justify-content-center fw-bold rounded-1 px-2 py-1`}>{title}</div>
+  )
+}
+
+//props defaultProps
+Header.defaultProps = {
+  enableBgColor: true
+};
+
+//props types
+Header.propTypes = {
+    title: PropTypes.string,
+    enableBgColor: PropTypes.bool
+  };
+export default Header
